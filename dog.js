a81ac9d468f6ad1e431cf0ff4e1216fe4091e311
Karthik-sairaa-21 <karthiksakthivel212003@gmail.com>
2024-09-10 16:32:10 +0530

    new files upload

--- dog.js
+++ dog.js
@@ -1,8 +1,8 @@
-// let char = 10;
-// if (char==2 && ) {
-//   console.log('The character is a.');
-// } else if (char === 'b') {
-//   console.log('The character is b.');
-// } else {
-//   console.log('The character is something else.');
-// }
+let char = 'A';
+if (char === 'a') {
+  console.log('The character is a.');
+} else if (char === 'b') {
+  console.log('The character is b.');
+} else {
+  console.log('The character is something else.');
+}
--- switch/switch-1.js
+++ switch/switch-1.js
@@ -1,29 +1,48 @@
-let day = 9;
-switch (day) {
-    case 1:
-        console.log("Today is Monday");
+// let day = "9";
+// switch (day) {
+//     case 1:
+//         console.log("Today is Monday");
+//         break;
+//     case 2:
+//         console.log("Today is Tuesday");
+//         break;
+//     case 3:
+//         console.log("Today is Wednesday");
+//         break;
+//     case 4:
+//         console.log("Today is Thursday");
+//         break;
+//     case 5:
+//         console.log("Today is Friday");
+//         break;
+//     case 6:
+//         console.log("Today is Saturday");
+//         break;
+//     case 7:
+//         console.log("Today is Sunday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+//         break;
+// }
+
+// console.log("karthik")
+
+let trafficLight = "green";
+// let message = ""
+ 
+switch (trafficLight) {
+    case "red":
+        message = "Stop immediately.";
         break;
-    case 2:
-        console.log("Today is Tuesday");
+    case "yellow":
+        message = "Prepare to stop.";
         break;
-    case 3:
-        console.log("Today is Wednesday");
-        break;
-    case 4:
-        console.log("Today is Thursday");
-        break;
-    case 5:
-        console.log("Today is Friday");
-        break;
-    case 6:
-        console.log("Today is Saturday");
-        break;
-    case 7:
-        console.log("Today is Sunday");
+    case "green":
+        message = "Proceed or continue driving.";
         break;
     default:
-        console.log("Invalid day");
-        break;
+        message = "Invalid traffic light color.";
 }
-
-console.log("karthik")
+ 
+console.log(message);
